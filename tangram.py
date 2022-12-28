201629d4b1ae6d723adddfb4985f118c7dd10254
Aydin Schwartz <aschwartz8@dons.usfca.edu>
2022-12-27 22:45:04 -0800

    add better visualization of individual blocks

--- tangram.py
+++ tangram.py
@@ -1,14 +1,31 @@
 import copy
 
 
+color_map = {
+    1: "⬜",
+    2: "🟥",
+    3: "🟧",
+    4: "🟩",
+    5: "🟦",
+    6: "🟪",
+    7: "🟫",
+    8: "🧿",
+    9: "🌕",
+    10: "👁",
+    11: "🤢",
+    12: "😎",
+    13: "💀"
+}
+
+
 def draw_board(board):
     for row in board:
         out_row = []
         for cell in row:
-            if cell == 1:
-                out_row.append("⬜")
-            elif cell == 0:
+            if cell == 0:
                 out_row.append("⬛")
+            else:
+                out_row.append(color_map[cell])
         print(" ".join(out_row))
     print()
 
@@ -24,9 +41,9 @@ def add_piece(board, piece, start_row, start_col, draw=False):
     for i, row in enumerate(piece):
         for j, val in enumerate(row):
             # only add filled spaces, never take away
-            if val == 1:
+            if val != 0:
                 # don't overwrite existing pieces on the board
-                if board[start_row + i][start_col + j] == 1:
+                if board[start_row + i][start_col + j] != 0:
                     legal_move = False
                     return board, legal_move
                 else:
@@ -93,52 +110,52 @@ pieces = {
                [0, 1, 1],
                [0, 0, 1]],
 
-    "right angle": [[1, 0, 0],
-                    [1, 0, 0],
-                    [1, 1, 1]],
+    "right angle": [[2, 0, 0],
+                    [2, 0, 0],
+                    [2, 2, 2]],
 
-    "plus": [[0, 1, 0],
-             [1, 1, 1],
-             [0, 1, 0]],
+    "plus": [[0, 3, 0],
+             [3, 3, 3],
+             [0, 3, 0]],
 
-    "long": [[1, 1, 1, 1, 1]],
+    "long": [[4, 4, 4, 4, 4]],
 
-    "big L": [[1, 0],
-              [1, 0],
-              [1, 0],
-              [1, 1]],
+    "big L": [[5, 0],
+              [5, 0],
+              [5, 0],
+              [5, 5]],
 
-    "square": [[1, 1],
-               [1, 1]],
+    "square": [[6, 6],
+               [6, 6]],
 
-    "T": [[1, 1, 1],
-          [0, 1, 0],
-          [0, 1, 0]],
+    "T": [[7, 7, 7],
+          [0, 7, 0],
+          [0, 7, 0]],
 
-    "arch": [[1, 1, 1],
-             [1, 0, 1]],
+    "arch": [[8, 8, 8],
+             [8, 0, 8]],
 
-    "house": [[1, 0],
-              [1, 1],
-              [1, 1]],
+    "house": [[9, 0],
+              [9, 9],
+              [9, 9]],
 
-    "twist": [[1, 1, 0],
-              [0, 1, 1],
-              [0, 1, 0]],
+    "twist": [[10, 10, 0],
+              [0,  10, 10],
+              [0,  10, 0]],
 
-    "tree": [[1, 0],
-             [1, 1],
-             [0, 1],
-             [0, 1]],
+    "tree": [[11, 0],
+             [11, 11],
+             [0,  11],
+             [0,  11]],
 
-    "arm": [[1, 0],
-            [1, 1],
-            [1, 0],
-            [1, 0]],
+    "arm": [[12, 0],
+            [12, 12],
+            [12, 0],
+            [12, 0]],
 
-    "S": [[1, 1, 0],
-          [0, 1, 0],
-          [0, 1, 1]]
+    "S": [[13, 13, 0],
+          [0,  13, 0],
+          [0,  13, 13]]
 }
 
 sub_pieces = {
@@ -147,18 +164,18 @@ sub_pieces = {
              [0, 1],
              [0, 1]],
 
-    "arm": [[1, 0],
-            [1, 1],
-            [1, 0],
-            [1, 0]],
+    "arm": [[2, 0],
+            [2, 2],
+            [2, 0],
+            [2, 0]],
 
-    "T": [[1, 1, 1],
-          [0, 1, 0],
-          [0, 1, 0]],
+    "T": [[3, 3, 3],
+          [0, 3, 0],
+          [0, 3, 0]],
 
-    "right angle": [[1, 0, 0],
-                    [1, 0, 0],
-                    [1, 1, 1]]
+    "right angle": [[4, 0, 0],
+                    [4, 0, 0],
+                    [4, 4, 4]]
 }
 
 
@@ -171,12 +188,12 @@ def test_positions():
 full_board = [[0, 0, 0, 0, 0, 0, 0, 0].copy() for _ in range(8)]
 half_board = [[0, 0, 0, 0, 0].copy() for _ in range(4)]
 pieces = gen_piece_positions(pieces)
-pieces = gen_piece_positions(sub_pieces)
+# pieces = gen_piece_positions(sub_pieces)
 
 
 def solve_board(board, pieces):
     # win condition is whole board is covered in pieces
-    draw_board(board)
+    # draw_board(board)
     if all([all(row) for row in board]):
         draw_board(board)
         return board
@@ -188,7 +205,7 @@ def solve_board(board, pieces):
                 solve_board(add_piece(board, position, row, col)[0], pieces[1:])
 
 
-solve_board(half_board, list(pieces.values()))
+solve_board(full_board, list(pieces.values()))
 
 # for name, group in pieces.items():
 #     print(name)
